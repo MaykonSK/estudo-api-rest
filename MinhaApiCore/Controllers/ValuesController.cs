49e3a425559cfced07ee02517cdb009ba105a6ef
Maycon <mayconrodrigo123@hotmail.com>
2022-02-03 14:09:20 -0300

    commit inicial

---
+++ MinhaApiCore/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet] //verbo
+        public ActionResult<IEnumerable<string>> ObterTodos()
+        {
+           var valores =  new string[] { "value1", "value2", "value3" };
+            if (valores.Length < 3)
+            {
+                return BadRequest(); //erro 400
+            } else
+            {
+                return Ok(valores); //success 200
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("obter-id/{id:int}")] //verbo
+        public ActionResult<string> ObterId(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost] //verbo
+        public void Post(Produtos value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")] //verbo com rota
+        public void Put(int id, [FromForm] Produtos value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")] //verbo
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Produtos
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public double valor { get; set; }
+    }
+}
